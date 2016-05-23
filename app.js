a2818d7b6286012582e3e5f530251de158be6c77
Andrey Golovin <caldwell@inbox.ru>
2016-05-23 17:20:44 +0500

    sass support added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var sassMiddleware = require('node-sass-middleware');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -20,6 +21,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(sassMiddleware({
+  /* Options */
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public/stylesheets'),
+  debug: true,
+  outputStyle: 'compressed',
+  prefix:  '/stylesheets'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
